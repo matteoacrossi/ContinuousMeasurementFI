af6d5bc815d5542cb42e0a086e7b66ca16b3ed1b
Matteo Rossi <teo.red90@gmail.com>
2018-07-27 12:06:28 +0200

    Rewritten the last function

--- Eff_QFI_HD.jl
+++ Eff_QFI_HD.jl
@@ -29,41 +29,43 @@ vectors containing the FI and average QFI
 function Eff_QFI_HD(Nj::Int64,          # Number of spins
     Ntraj::Int64,                       # Number of trajectories
     Tfinal::Float64,                    # Final time
-    dt::Float64;                        # Time step
-    κ = 1.,                             # Noise coupling
-    θ = 0.,                             # Noise angle
-    ω = 0.,                             # Frequency
+    dt::Float64,                        # Time step
+    H, dH,                              # Hamiltonian and its derivative wrt ω
+    non_monitored_noise_op,             # Non monitored noise operators
+    monitored_noise_op;                 # Monitored noise operators
+    initial_state = ghz_state,          # Initial state
     η = 1.)                             # Measurement efficiency
 
     Ntime = Int(floor(Tfinal/dt)) # Number of timesteps
     dimJ = Int(2^Nj)   # Dimension of the corresponding Hilbert space
 
-    # Define an array of noise channels
-    cj = sqrt(κ/2) *
-        [(cos(θ) * σ_j(:z, j, Nj) + sin(θ) * σ_j(:x, j, Nj)) for j = 1:Nj]
+    # Non-monitored noise operators
+    # cj = [] if all the noise is monitored
+    cj = non_monitored_noise_op
+    Nnm = length(cj)
+
+    # Monitored noise operators
+    Cj = monitored_noise_op
+    Nm = length(Cj)
 
     # We store the operators sums for efficiency
-    cjSum = [(c + c') for c in cj]
+    CjSum = [(c + c') for c in Cj]
 
     # We store the operators products for efficiency
-    cjProd = [cj[i]*cj[j] for i in eachindex(cj), j in eachindex(cj)]
+    CjProd = [cj[i]*cj[j] for i in eachindex(Cj), j in eachindex(Cj)]
 
-    # ω is the parameter that we want to estimate
-    H = ω * σ(:z, Nj) / 2       # Hamiltonian of the spin system
-    dH = σ(:z, Nj) / 2          # Derivative of H wrt the parameter ω
-
-    dW() = sqrt(dt) * randn(Nj) # Define the Wiener increment vector
+    dW() = sqrt(dt) * randn(Nm) # Define the Wiener increment vector
 
     # Kraus-like operator, trajectory-independent part
-
-    M0 = sparse(speye(dimJ) - 1im * H * dt -
-                0.5 * dt * sum([c' * c for c in cj]))
+    M0 = sparse(speye(dimJ) - 1im * H * dt
+                - 0.5 * dt * sum([c' * c for c in cj])
+                - 0.5 * dt * sum([C'*C for C in Cj]))
 
     # Derivative of the Kraus-like operator wrt to ω
     dM = -1im * dH * dt
 
     # Initial state of the system
-    ψ0 = ghz_state(Nj)
+    ψ0 = initial_state(Nj)
     ρ0 = ψ0 * ψ0'
 
     t = (1 : Ntime) * dt
@@ -85,17 +87,17 @@ function Eff_QFI_HD(Nj::Int64,          # Number of spins
         for jt = 1 : Ntime
 
             # Homodyne current (Eq. 35)
-            dy = dt * sqrt(η) * [trace(ρ * c) for c in cjSum] + dW()
+            dy = dt * sqrt(η) * [trace(ρ * C) for C in CjSum] + dW()
 
             # Kraus operator Eq. (36)
             M = M0 +
-                sqrt(η) * sum([cj[j] * dy[j] for j = 1:Nj]) +
-                η/2 * sum([cjProd[i,j] *(dy[i] * dy[j] - (i == j ? dt : 0)) for i = 1:Nj, j = 1:Nj])
+                sqrt(η) * sum([Cj[j] * dy[j] for j = 1:Nm]) +
+                η/2 * sum([CjProd[i,j] *(dy[i] * dy[j] - (i == j ? dt : 0)) for i = 1:Nm, j = 1:Nm])
 
 
             # Evolve the density operator
             new_ρ = M * ρ * M' +
-                     (1 - η) * dt * sum([c * ρ * c' for c in cj])
+                     (1 - η) * dt * sum([C * ρ * C' for C in Cj])
 
             zchop!(new_ρ) # Round off elements smaller than 1e-14
 
@@ -103,7 +105,7 @@ function Eff_QFI_HD(Nj::Int64,          # Number of spins
 
             # Evolve the unnormalized derivative wrt ω
             τ = (M * (τ * M' + ρ * dM') + dM * ρ * M' +
-                   (1 - η)* dt * sum([c * τ * c' for c in cj])
+                   (1 - η)* dt * sum([C * τ * C' for C in Cj])
                   )/ tr_ρ
 
             zchop!(τ) # Round off elements smaller than 1e-14
