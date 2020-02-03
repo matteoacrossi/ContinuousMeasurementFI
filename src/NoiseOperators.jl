4f26fff432bfe802bae551f36b4f376c70c958b4
Matteo Rossi <teo.red90@gmail.com>
2020-02-03 10:58:04 +0200

    Added utility functions

--- src/NoiseOperators.jl
+++ src/NoiseOperators.jl
@@ -4,6 +4,8 @@ Functions for constructing noise operators
 using SparseArrays
 using LinearAlgebra
 import SparseArrays.getcolptr
+using BlockDiagonals
+
 
 """
     σ_j(j, n, direction)
@@ -181,4 +183,142 @@ function fast_sup_post!(Sup::SparseMatrixCSC{T1, S1}, A::SparseMatrixCSC{T1,S1})
         end
     end
     return Sup
+end
+
+"""
+Contains the block, row and column indices for efficient application of the superoperator
+"""
+# TODO: Format using 2 tuples
+struct Indices{T}
+    br::Array{T, 1}
+    bc::Array{T, 1}
+    ir::Array{T, 1}
+    jr::Array{T, 1}
+    ic::Array{T, 1}
+    jc::Array{T, 1}
+end
+
+"""
+    apply_superop!(C::BlockDiagonal, A::SparseMatrixCSC{Ti, Tv}, B::BlockDiagonal) -> C
+
+Apply the superoperator A (in Liouville form) to the matrix B and store the result in C.
+
+"""
+function apply_superop!(C::BlockDiagonal, A::SparseMatrixCSC{Ti, Tv}, B::BlockDiagonal) where Ti where Tv
+    return apply_superop!(C, A, B, get_superop_indices(A, B))
+end
+
+""" get_superop_indices(A::SparseMatrixCSC{Ti, Tv}, B::BlockDiagonal)
+
+Returns the Indices structure for the action of operator A onto the matrix B
+"""
+function get_superop_indices(A::SparseMatrixCSC{Ti, Tv}, B::BlockDiagonal) where Ti where Tv
+
+    N = size(B, 1)
+    row, col, val = findnz(A)
+    bs = first.(blocksizes(B))
+
+    blockindices = cumsum(vcat(1, bs[1:end-1]))
+
+    irow = (row .- 1) .% N .+ 1
+    jrow = (row .- 1) .÷ N .+ 1
+    ir = similar(irow)
+    jr = similar(jrow)
+
+    t1 = sum(Int.(floor.(reshape(irow, :, 1) ./ blockindices[2:end]')) .>= 1, dims=2) .+ 1
+    t2 = sum(Int.(floor.(reshape(jrow, :, 1) ./ blockindices[2:end]')) .>= 1, dims=2) .+ 1
+
+    br = t1 .* (t1 .== t2)
+
+    @simd for i = 1:length(irow)
+        if br[i] > 0
+            ir[i] = irow[i] - blockindices[br[i]] + 1
+            jr[i] = jrow[i] - blockindices[br[i]] + 1
+        end
+    end
+
+    icol = (col .- 1) .% N .+ 1
+    jcol = (col .- 1) .÷ N .+ 1
+
+    ic = similar(icol)
+    jc = similar(jcol)
+
+    t1 .= sum(Int.(floor.(reshape(icol, :, 1) ./ blockindices[2:end]')) .>= 1, dims=2) .+ 1
+    t2 .= sum(Int.(floor.(reshape(jcol, :, 1) ./ blockindices[2:end]')) .>= 1, dims=2) .+ 1
+
+    bc = t1 .* (t1 .== t2)
+
+    @simd for i = 1:length(icol)
+        if bc[i] > 0
+            ic[i] = icol[i] - blockindices[bc[i]] + 1
+            jc[i] = jcol[i] - blockindices[bc[i]] + 1
+        end
+    end
+
+    return Indices(br[:], bc[:], ir, jr, ic, jc)
+end
+
+"""
+    apply_superop!(C::BlockDiagonal, A::SparseMatrixCSC{Ti, Tv}, B::BlockDiagonal, indices::Indices) -> C
+
+Apply the superoperator A (in Liouville form) to the matrix B and store the result in C, using the provided
+indices for much faster operation
+"""
+function apply_superop!(C::BlockDiagonal, A::SparseMatrixCSC{Ti, Tv}, B::BlockDiagonal, indices::Indices{Tv}) where Ti where Tv
+        val = nonzeros(A)
+        fill!(C, zero(eltype(C)))
+        @simd for i = 1 : length(val)
+            if indices.bc[i] > 0
+                tmp = val[i] * B.blocks[indices.bc[i]][indices.ic[i], indices.jc[i]]
+                if tmp != 0.0
+                    C.blocks[indices.br[i]][indices.ir[i], indices.jr[i]] += tmp
+                end
+            end
+        end
+        return C
+    end
+
+import ZChop: zchop!
+# Specialize zchop! to BlockDiagonal
+function zchop!(A::BlockDiagonal)
+   for b in blocks(A)
+        zchop!(b)
+    end
+end
+
+""" nspins(size)
+
+Obtain the number of spins from the size of a matrix in the
+Dicke basis
+"""
+function nspins(size)
+    if sqrt(size) == floor(sqrt(size)) # Nspin even
+         return 2 * (Int(sqrt(size)) - 1)
+     else # Nspin odd
+         return -2 + Int(sqrt(1 + 4 * size))
+     end
+ end
+
+""" blockdiagonal(M; [dense=false])
+
+Converts a matrix into a BlockDiagonal matrix, by default preserving the matrix type (e.g. sparse, dense).
+If dense=true, force the blocks to have a dense structure
+"""
+function blockdiagonal(M::T; dense=false) where T <: AbstractArray
+    N = nspins(size(M, 1))
+
+    blocksizes = ContinuousMeasurementFI.block_sizes(N)
+
+    views = Array{T}(undef, length(blocksizes))
+    startidx = 1
+    for i in 1:length(blocksizes)
+        range = startidx:(startidx + blocksizes[i] - 1)
+        views[i] = view(M, range, range)
+        startidx += blocksizes[i]
+    end
+    if dense # Force dense blocks
+        return BlockDiagonal(Matrix.(views))
+    else
+        return BlockDiagonal(views)
+    end
 end
