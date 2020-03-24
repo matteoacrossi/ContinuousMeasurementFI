61b1a38021e8504eb25ae5419878ea8694073c6b
Matteo Rossi <teo.red90@gmail.com>
2020-03-24 11:18:23 +0200

    Small tidying

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,4 @@
+using ZChop
 function squeezing_param(N, ΔJ1, J2m, J3m)
     """
         ξ2 = squeezing_param(N, ΔJ1, J2m, J3m)
@@ -8,6 +9,11 @@ function squeezing_param(N, ΔJ1, J2m, J3m)
     return (J2m .^2 + J3m .^2) ./ (N * ΔJ1)
 end
 
+"""
+    density(M)
+
+Returns the density of a sparse matrix
+"""
 function density(s::SparseMatrixCSC)
     return length(s.nzval) / (s.n * s.m)
 end
@@ -19,3 +25,10 @@ function Unconditional_QFI_Dicke(Nj::Int64, Tfinal::Real, dt::Real;
     )
     return Eff_QFI_HD_Dicke(Nj, 1, Tfinal, dt; κ=κ, κcoll = κcoll, ω=ω, η=0.0)
 end
+
+# Specialize zchop! to BlockDiagonal
+function ZChop.zchop!(A::BlockDiagonal)
+    for b in blocks(A)
+         zchop!(b)
+     end
+ end
